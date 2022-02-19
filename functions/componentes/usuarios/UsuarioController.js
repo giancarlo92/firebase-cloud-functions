f05efb0c37b28665518eadc033fd8c1ab8fce31a
GIANCARLO ZEVALLOS LECCA <g10n_22_92@hotmail.com>
2022-02-19 00:37:00 -0500

    first commit

---
+++ functions/componentes/usuarios/UsuarioController.js
@@ -0,0 +1,13 @@
+const { UsuarioAdmin } = require('./UsuarioAdmin')
+
+exports.usuarioCreacionController = usuario => {
+    
+}
+
+exports.usuarioEliminadoController = usuario =>  {
+
+}
+
+exports.creacionUsuarioCRM = usuario => {
+
+}
---
+++ functions/index.js
@@ -0,0 +1,2 @@
+const functions = require('firebase-functions')
+const usuarioController = require('./componentes/usuarios/UsuarioController.js')
